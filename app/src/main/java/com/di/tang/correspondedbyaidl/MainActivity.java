194c70553ee36aed98a70a2e9f1b79d18ca1fbbe
ChinaTang <tangdi0802@foxmail.com>
2016-09-12 18:14:28 +0800

    微调

--- app/src/main/java/com/di/tang/correspondedbyaidl/MainActivity.java
+++ app/src/main/java/com/di/tang/correspondedbyaidl/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
     private static final int MESSAGE_NEW_ARRIVED = 0x123;
 
     private IBookManager mRemoteBookManager;
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+
+        }
+    };
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
